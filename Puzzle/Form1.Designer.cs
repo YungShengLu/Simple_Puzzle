c3d5c940e385a2890e55ed8b32bc98891404c66f
unknown <>
2016-05-06 15:05:50 +0800

    20160506 - V.1

---
+++ Puzzle/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Puzzle {
+    partial class Main {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.puzzle_33 = new System.Windows.Forms.PictureBox();
+            this.puzzle_23 = new System.Windows.Forms.PictureBox();
+            this.puzzle_32 = new System.Windows.Forms.PictureBox();
+            this.puzzle_31 = new System.Windows.Forms.PictureBox();
+            this.puzzle_22 = new System.Windows.Forms.PictureBox();
+            this.puzzle_21 = new System.Windows.Forms.PictureBox();
+            this.puzzle_13 = new System.Windows.Forms.PictureBox();
+            this.puzzle_12 = new System.Windows.Forms.PictureBox();
+            this.puzzle_11 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label_timer = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_33)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_23)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_32)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_31)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_11)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.puzzle_33);
+            this.panel1.Controls.Add(this.puzzle_23);
+            this.panel1.Controls.Add(this.puzzle_32);
+            this.panel1.Controls.Add(this.puzzle_31);
+            this.panel1.Controls.Add(this.puzzle_22);
+            this.panel1.Controls.Add(this.puzzle_21);
+            this.panel1.Controls.Add(this.puzzle_13);
+            this.panel1.Controls.Add(this.puzzle_12);
+            this.panel1.Controls.Add(this.puzzle_11);
+            this.panel1.Location = new System.Drawing.Point(24, 67);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(240, 240);
+            this.panel1.TabIndex = 0;
+            // 
+            // puzzle_33
+            // 
+            this.puzzle_33.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.puzzle_33.Location = new System.Drawing.Point(159, 159);
+            this.puzzle_33.Name = "puzzle_33";
+            this.puzzle_33.Size = new System.Drawing.Size(80, 80);
+            this.puzzle_33.TabIndex = 5;
+            this.puzzle_33.TabStop = false;
+            // 
+            // puzzle_23
+            // 
+            this.puzzle_23.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.puzzle_23.Location = new System.Drawing.Point(159, 80);
+            this.puzzle_23.Name = "puzzle_23";
+            this.puzzle_23.Size = new System.Drawing.Size(80, 80);
+            this.puzzle_23.TabIndex = 5;
+            this.puzzle_23.TabStop = false;
+            // 
+            // puzzle_32
+            // 
+            this.puzzle_32.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.puzzle_32.Location = new System.Drawing.Point(80, 159);
+            this.puzzle_32.Name = "puzzle_32";
+            this.puzzle_32.Size = new System.Drawing.Size(80, 80);
+            this.puzzle_32.TabIndex = 4;
+            this.puzzle_32.TabStop = false;
+            // 
+            // puzzle_31
+            // 
+            this.puzzle_31.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.puzzle_31.Location = new System.Drawing.Point(1, 159);
+            this.puzzle_31.Name = "puzzle_31";
+            this.puzzle_31.Size = new System.Drawing.Size(80, 80);
+            this.puzzle_31.TabIndex = 3;
+            this.puzzle_31.TabStop = false;
+            // 
+            // puzzle_22
+            // 
+            this.puzzle_22.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.puzzle_22.Location = new System.Drawing.Point(80, 80);
+            this.puzzle_22.Name = "puzzle_22";
+            this.puzzle_22.Size = new System.Drawing.Size(80, 80);
+            this.puzzle_22.TabIndex = 4;
+            this.puzzle_22.TabStop = false;
+            // 
+            // puzzle_21
+            // 
+            this.puzzle_21.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.puzzle_21.Location = new System.Drawing.Point(1, 80);
+            this.puzzle_21.Name = "puzzle_21";
+            this.puzzle_21.Size = new System.Drawing.Size(80, 80);
+            this.puzzle_21.TabIndex = 3;
+            this.puzzle_21.TabStop = false;
+            // 
+            // puzzle_13
+            // 
+            this.puzzle_13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.puzzle_13.Location = new System.Drawing.Point(159, 1);
+            this.puzzle_13.Name = "puzzle_13";
+            this.puzzle_13.Size = new System.Drawing.Size(80, 80);
+            this.puzzle_13.TabIndex = 2;
+            this.puzzle_13.TabStop = false;
+            // 
+            // puzzle_12
+            // 
+            this.puzzle_12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.puzzle_12.Location = new System.Drawing.Point(80, 1);
+            this.puzzle_12.Name = "puzzle_12";
+            this.puzzle_12.Size = new System.Drawing.Size(80, 80);
+            this.puzzle_12.TabIndex = 1;
+            this.puzzle_12.TabStop = false;
+            // 
+            // puzzle_11
+            // 
+            this.puzzle_11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.puzzle_11.Location = new System.Drawing.Point(1, 1);
+            this.puzzle_11.Name = "puzzle_11";
+            this.puzzle_11.Size = new System.Drawing.Size(80, 80);
+            this.puzzle_11.TabIndex = 0;
+            this.puzzle_11.TabStop = false;
+            this.puzzle_11.Click += new System.EventHandler(this.puzzle_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(18, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Puzzle";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(183, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(305, 92);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(178, 214);
+            this.listBox1.Sorted = true;
+            this.listBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(301, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Record";
+            // 
+            // label_timer
+            // 
+            this.label_timer.AutoSize = true;
+            this.label_timer.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label_timer.Location = new System.Drawing.Point(398, 28);
+            this.label_timer.Name = "label_timer";
+            this.label_timer.Size = new System.Drawing.Size(65, 24);
+            this.label_timer.TabIndex = 5;
+            this.label_timer.Text = "label3";
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(25, 331);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 25);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(305, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 32);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(505, 382);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label_timer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(521, 421);
+            this.MinimumSize = new System.Drawing.Size(521, 421);
+            this.Name = "Main";
+            this.Text = "Puzzle";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_33)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_23)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_32)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_31)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.puzzle_11)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox puzzle_33;
+        private System.Windows.Forms.PictureBox puzzle_23;
+        private System.Windows.Forms.PictureBox puzzle_32;
+        private System.Windows.Forms.PictureBox puzzle_31;
+        private System.Windows.Forms.PictureBox puzzle_22;
+        private System.Windows.Forms.PictureBox puzzle_21;
+        private System.Windows.Forms.PictureBox puzzle_13;
+        private System.Windows.Forms.PictureBox puzzle_12;
+        private System.Windows.Forms.PictureBox puzzle_11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_timer;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Puzzle/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Puzzle {
+    public struct Position {
+        public int x, y;
+    };
+
+    public partial class Main : Form {
+        private string player;
+        private Image[] image;
+        private PuzzleBlock[,] puzzle;
+        private PictureBox[,] table;
+        private int[] randomList;
+        private Random rand;
+        private TimeSpan start;
+
+        public Main() {
+            InitializeComponent();
+
+            // Initialize.
+            image = new Image[9];
+            table = new PictureBox[3, 3];
+            randomList = new int[9];
+            rand = new Random();
+            start = new TimeSpan(0);            
+
+            puzzle = new PuzzleBlock[3, 3];
+            Position currPosition = new Position();
+            for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 3; ++j) {
+                    currPosition.x = i;
+                    currPosition.y = j;
+
+                    puzzle[i, j] = new PuzzleBlock();
+                    puzzle[i, j].setPosition(currPosition);
+                }
+            }
+
+            table[0, 0] = puzzle_11;
+            table[0, 1] = puzzle_12;
+            table[0, 2] = puzzle_13;
+            table[1, 0] = puzzle_21;
+            table[1, 1] = puzzle_22;
+            table[1, 2] = puzzle_23;
+            table[2, 0] = puzzle_31;
+            table[2, 1] = puzzle_32;
+            table[2, 2] = puzzle_33;
+
+            // Subscribe.
+            for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 3; ++j) {
+                    puzzle[i, j].setPuzzle(table[i, j]);
+                    puzzle[i, j].getPuzzle().Click += puzzle_Click;
+                }
+            }
+
+            image[0] = Resource1._1;
+            image[1] = Resource1._2;
+            image[2] = Resource1._3;
+            image[3] = Resource1._4;
+            image[4] = Resource1._5;
+            image[5] = Resource1._6;
+            image[6] = Resource1._7;
+            image[7] = Resource1._8;
+            image[8] = Resource1._9;
+        }
+
+        private void Main_Load(object sender, EventArgs e) {
+            label_timer.Text = "00:00:00";
+
+            for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 3; ++j)
+                    puzzle[i, j].setImage(image[8]);
+            }
+        }
+
+        private void puzzle_Click(object sender, EventArgs e) {
+            PictureBox curr = (PictureBox)sender;
+            Position currPosition = getCurrPosition(curr);
+
+            Console.WriteLine("{0} {1}", currPosition.x, currPosition.y);
+
+            if (currPosition.x + 1 < 3 && puzzle[currPosition.x + 1, currPosition.y].getImage() == image[8]) {
+                puzzle[currPosition.x + 1, currPosition.y].setImage(curr.Image);
+                puzzle[currPosition.x, currPosition.y].setImage(image[8]);
+            }
+            else if (currPosition.x - 1 >= 0 && puzzle[currPosition.x - 1, currPosition.y].getImage() == image[8]) {
+                puzzle[currPosition.x - 1, currPosition.y].setImage(curr.Image);
+                puzzle[currPosition.x, currPosition.y].setImage(image[8]);
+            }
+            else if (currPosition.y + 1 < 3 && puzzle[currPosition.x, currPosition.y + 1].getImage() == image[8]) {
+                puzzle[currPosition.x, currPosition.y + 1].setImage(curr.Image);
+                puzzle[currPosition.x, currPosition.y].setImage(image[8]);
+            }
+            else if (currPosition.y - 1 >= 0 && puzzle[currPosition.x, currPosition.y - 1].getImage() == image[8]) {
+                puzzle[currPosition.x, currPosition.y - 1].setImage(curr.Image);
+                puzzle[currPosition.x, currPosition.y].setImage(image[8]);
+            }
+        }
+
+        public Position getCurrPosition(PictureBox curr) {
+            Position currPosition = new Position();
+
+            for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 3; ++j) {
+                    if (puzzle[i, j].getImage() == curr.Image) {
+                        currPosition = puzzle[i, j].getPosition();
+                        return currPosition;
+                    }
+                }
+            }
+
+            return currPosition;
+        }
+
+        private bool isFinished() {
+            for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 3; ++j) {
+                    if (puzzle[i, j].getImage() != image[i * 3 + j])
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            player = textBox1.Text;
+
+            while (player == "") {
+                MessageBox.Show("Please enter your name.", "Puzzle");
+                return;
+            }
+
+            // Random the puzzle.
+            for (int i = 0; i < 9; ++i) {
+                randomList[i] = rand.Next(1, 10);
+                for (int j = 0; j < i; ++j) {
+                    while (randomList[i] == randomList[j]) {
+                        j = 0;
+                        randomList[i] = rand.Next(1, 10);
+                    }
+                }
+            }
+
+            // Test for finishing.
+            
+            for (int i = 0; i < 7; ++i) {
+                randomList[i] = i + 1;
+            }
+
+            randomList[7] = 9;
+            randomList[8] = 8;
+            
+
+            // Set the puzzle.
+            for (int i = 0; i < 3; ++i) {
+                for (int j = 0; j < 3; ++j) {
+                    puzzle[i, j].setImage(image[randomList[i * 3 + j] - 1]);
+                    Console.Write("{0} ", randomList[i * 3 + j] - 1);
+                }
+                Console.WriteLine("");
+            }
+
+            // Set timer.
+            timer.Enabled = true;
+        }
+
+        private void timer_Tick(object sender, EventArgs e) {
+            start += TimeSpan.FromMilliseconds(100);
+            label_timer.Text = start.ToString(@"hh\:mm\:ss");
+
+            DialogResult result = new DialogResult();
+            if (isFinished()) {
+                timer.Stop();
+
+                result = MessageBox.Show("Finish!", "Puzzle", MessageBoxButtons.OK);
+                if (result == DialogResult.OK) {
+                    textBox1.Text = "";
+                    listBox1.Items.Add(player + "  " + label_timer.Text);
+                }
+
+                label_timer.Text = "00:00:00";
+                timer.Enabled = false;
+                timer.Interval = 100;
+                start = new TimeSpan(0);
+
+                for (int i = 0; i < 3; ++i) {
+                    for (int j = 0; j < 3; ++j)
+                        puzzle[i, j].setImage(image[8]);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            DialogResult result = MessageBox.Show("Puzzle", "Are you sure to delete the record?", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes) {
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
+        }
+    }
+
+    public class PuzzleBlock {
+        private PictureBox picturebox;
+        private Position position;
+
+        public PuzzleBlock() {
+            picturebox = new PictureBox();
+            position = new Position();
+        }
+
+        public void setImage(Image image) {
+            this.picturebox.Image = image;
+        }
+
+        public Image getImage() {
+            return this.picturebox.Image;
+        }
+
+        public void setPuzzle(PictureBox picturebox) {
+            this.picturebox = picturebox;
+        }
+
+        public PictureBox getPuzzle() {
+            return this.picturebox;
+        }
+
+        public void setPosition(Position curr_position) {
+            this.position = curr_position;
+        }
+
+        public Position getPosition() {
+            return this.position;
+        }
+    }
+}
